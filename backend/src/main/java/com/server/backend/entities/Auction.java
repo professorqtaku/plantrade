4772a7680b9577da83d6e512dc4b217fd17ca372
oscargregorius <oscar.gregorius@gmail.com>
2021-10-25 14:28:15 +0200

    Added non nullable to some fields.

--- backend/src/main/java/com/server/backend/entities/Auction.java
+++ backend/src/main/java/com/server/backend/entities/Auction.java
@@ -21,8 +21,13 @@ public class Auction {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(nullable = false)
     private String title;
+
+    @Column(nullable = false)
     private String description;
+
+    @Column(nullable = false)
     private Integer startPrice;
     private Status status;
     private Date endDate;
@@ -30,43 +35,43 @@ public class Auction {
     @ManyToOne
     private User host;
 
-  @ManyToMany(cascade = CascadeType.DETACH)
-  @JoinTable(
-        name = "AuctionXCategory",
-        joinColumns = @JoinColumn(name = "auction_id"),
-        inverseJoinColumns = @JoinColumn(name = "category_id")
-    )
-    private List<Category> categories;
+  //@ManyToMany(cascade = CascadeType.DETACH)
+  //@JoinTable(
+  //      name = "AuctionXCategory",
+  //      joinColumns = @JoinColumn(name = "auction_id"),
+  //      inverseJoinColumns = @JoinColumn(name = "category_id")
+  //  )
+  //  private List<Category> categories;
+//
+//  @OneToMany(mappedBy = "auction_id", cascade = CascadeType.REMOVE)
+//    private List<Bid> bids;
+//
+//     @OneToMany(mappedBy = "auction", cascade = CascadeType.REMOVE)
+//     private List<Image> images;
+//
+//     public void addCategory(Category category) {
+//         categories.add(category);
+//    }
+//
+//     public void removeCategory(Category category) {
+//         categories.remove(category);
+    //    }
+//
+    //    public void addImage(Image image) {
+    //        images.add(image);
+    //    }
 
-    @OneToMany(mappedBy = "auction_id", cascade = CascadeType.REMOVE)
-   private List<Bid> bids;
+    //    public void removeImage(Image image) {
+//         images.remove(image);
+//     }
 
-    @OneToMany(mappedBy = "auction", cascade = CascadeType.REMOVE)
-    private List<Image> images;
+    //    public void addBid(Bid bid) {
+    //      bids.add(bid);
+//     }
 
-    public void addCategory(Category category) {
-        categories.add(category);
-   }
-
-    public void removeCategory(Category category) {
-        categories.remove(category);
-    }
-
-    public void addImage(Image image) {
-        images.add(image);
-    }
-
-    public void removeImage(Image image) {
-        images.remove(image);
-    }
-
-    public void addBid(Bid bid) {
-        bids.add(bid);
-    }
-
-    public void removeBid(Bid bid) {
-        bids.remove(bid);
-    }
+    //   public void removeBid(Bid bid) {
+    //       bids.remove(bid);
+    //    }
 
 
 
