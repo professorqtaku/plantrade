df1a212646b7bd1ff04fdb59bc18312234f75ad6
oscargregorius <oscar.gregorius@gmail.com>
2021-10-25 16:00:12 +0200

    Return null if the incomming dates are not valid

--- backend/src/main/java/com/server/backend/services/AuctionService.java
+++ backend/src/main/java/com/server/backend/services/AuctionService.java
@@ -5,6 +5,7 @@ import com.server.backend.entities.User;
 import com.server.backend.entities.Status;
 import com.server.backend.repositories.AuctionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -17,15 +18,20 @@ public class AuctionService {
     @Autowired
     private AuctionRepository auctionRepository;
 
-    public Auction createAuction(Auction auction){
+    public Auction createAuction(Auction auction, User user) {
         Date date = new Date();
         var inputDate = auction.getEndDate().getTime();
         Long oneDayinMillis = date.getTime() + 86400000;
         Long oneMonthInMillis = date.getTime() + 2592000000L;
 
-        if(inputDate < oneDayinMillis || inputDate > oneMonthInMillis){
-
+        if (inputDate < oneDayinMillis || inputDate > oneMonthInMillis) {
+            return null;
         }
+        auction.setStatus(Status.OPEN);
+        auction.setHost(user);
+        return auctionRepository.save(auction);
+
+    }
 
     public List<Auction> getAllAuctions() {
         return auctionRepository.findAll();
@@ -38,10 +44,5 @@ public class AuctionService {
     public List<Auction> getAuctionsByCurrentUser(User user) {
 
         return auctionRepository.findByHost(user);
-    }
-    public Auction createAuction(Auction auction, User user){
-        auction.setStatus(Status.OPEN);
-        auction.setHost(user);
-        return auctionRepository.save(auction);
     }
 }
