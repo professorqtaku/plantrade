50bf86e3ed9666b64b836d83729c6bf15910d115
oscargregorius <oscar.gregorius@gmail.com>
2021-10-25 19:32:12 +0200

    Added all the fields needed with design

--- frontend/src/Components/AuctionCard/AuctionCard.tsx
+++ frontend/src/Components/AuctionCard/AuctionCard.tsx
@@ -1,26 +1,30 @@
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import Avatar from "@mui/material/Avatar";
 import {
   StyledCard,
   StyledImg,
   StyledTitle,
   StyledDesc,
   StyledAvatar,
+  StyledButton,
+  StyledCardContent,
 } from "./StyledAuctionCard";
 
 const AuctionCard = () => {
   return (
     <StyledCard>
       <StyledImg src="https://i.pinimg.com/564x/9e/8b/dc/9e8bdc74df3cb2f87fae194a18ba569a.jpg" />
-      <CardContent>
-        <StyledAvatar>N</StyledAvatar>
-        <StyledTitle>Title</StyledTitle>
-        <StyledDesc>Description...</StyledDesc>
-      </CardContent>
+      <StyledCardContent>
+        <div>
+          <StyledAvatar>N</StyledAvatar>
+          <StyledTitle>Title</StyledTitle>
+        </div>
+        <div>
+          <StyledDesc>Description..</StyledDesc>
+          <StyledDesc>Price..</StyledDesc>
+          <StyledDesc>Highest bid..</StyledDesc>
+          <StyledDesc>End date..</StyledDesc>
+        </div>
+        <StyledButton>Snabb bud</StyledButton>
+      </StyledCardContent>
     </StyledCard>
   );
 };
--- frontend/src/Components/AuctionCard/StyledAuctionCard.ts
+++ frontend/src/Components/AuctionCard/StyledAuctionCard.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Card from "@mui/material/Card";
 import Avatar from "@mui/material/Avatar";
+import CardContent from "@mui/material/CardContent";
 
 export const StyledCard = styled(Card)`
   max-width: 90%;
@@ -8,6 +9,11 @@ export const StyledCard = styled(Card)`
   margin: 0 auto;
   display: grid;
   grid-template-columns: 35% 1fr;
+`;
+
+export const StyledCardContent = styled(CardContent)`
+  display: grid;
+  grid-template-rows: 30% 1fr 20%;
 `;
 
 export const StyledImg = styled.img`
@@ -19,15 +25,26 @@ export const StyledTitle = styled.p`
   font-size: 1rem;
   text-transform: uppercase;
   letter-spacing: 1.5px;
-`
+  margin-top: 0.3rem;
+`;
 
 export const StyledDesc = styled.p`
   font-size: 0.5rem;
   text-transform: uppercase;
   letter-spacing: 1.5px;
+  margin-top: 0;
 `;
 
 export const StyledAvatar = styled(Avatar)`
   background: #619463;
   float: right;
+`;
+
+export const StyledButton = styled.button`
+  background: #619463;
+  color: white;
+  border: none;
+  text-transform: uppercase;
+  letter-spacing: 3px;
+  padding: 0.5rem;
 `;
