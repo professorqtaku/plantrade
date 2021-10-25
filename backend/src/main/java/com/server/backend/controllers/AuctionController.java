bab2f02db2b078c5ab9c6b8d3f3f4fdf27598bfc
bextex <r.sarvegard@gmail.com>
2021-10-25 10:55:41 +0200

    added autowired in service and controller

--- backend/src/main/java/com/server/backend/controllers/AuctionController.java
+++ backend/src/main/java/com/server/backend/controllers/AuctionController.java
@@ -1,9 +1,14 @@
 package com.server.backend.controllers;
 
+import com.server.backend.services.AuctionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/rest/auctions")
 public class AuctionController {
+
+    @Autowired
+    private AuctionService auctionService;
 }
