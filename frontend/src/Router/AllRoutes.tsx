a0215d88a54ee621df54bbbf0b71526349e25700
Ali94160 <ali.al-mousawi@hotmail.com>
2021-10-25 11:46:34 +0200

    Created AuctionPage with routing and Styled File.

--- frontend/src/Router/AllRoutes.tsx
+++ frontend/src/Router/AllRoutes.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AuctionPage from "../Pages/AuctionPage/AuctionPage";
 import HomePage from "../Pages/HomePage/HomePage";
 
 interface Props {
@@ -12,6 +13,7 @@ const AllRoutes: React.FC<Props> = ({children}) => {
       {children}
       <Switch>
         <Route path="/" exact={true} component={HomePage} />
+        <Route path="/auctions" exact={true} component={AuctionPage} />
       </Switch>
     </Router>
   );
