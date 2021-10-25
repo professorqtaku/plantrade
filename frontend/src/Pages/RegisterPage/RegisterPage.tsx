91cfa511343d21217295c7479b509ea03903c3d4
Ali94160 <ali.al-mousawi@hotmail.com>
2021-10-25 15:45:05 +0200

    Created input in form with useRef

--- frontend/src/Pages/RegisterPage/RegisterPage.tsx
+++ frontend/src/Pages/RegisterPage/RegisterPage.tsx
@@ -1,11 +1,22 @@
-
+import { Ref } from "react";
+import { RefObject } from "react";
+import { MutableRefObject } from "react";
+import { useRef } from "react";
 import { StyledWrapper } from "./StyledRegisterPage";
 
+
 const RegisterPage = () => {
+
+  const usernameRef = useRef<any>();
+  const emailRef = useRef<any>();
+  const passwordRef = useRef<any>();
 
   const handleRegister = (e: React.FormEvent) => {
     e.preventDefault();
  
+    const user = {
+
+    }
     // Create user-object and call on the register func from AuthContext.
   }
 
@@ -15,6 +26,11 @@ const RegisterPage = () => {
 
       <form onSubmit={(e) => handleRegister(e)}>
 
+        <input ref={usernameRef} type="text" placeholder="Username" />
+        <input ref={emailRef} type="text" placeholder="Email" />
+        <input ref={passwordRef} type="text" placeholder="Password" />
+
+        <button>Skapa Konto</button>
       </form>
     </StyledWrapper>
   );
