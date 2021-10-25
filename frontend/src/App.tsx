6da49e8196ae2fafc93c1df112f8439da06e9fcc
Ali94160 <ali.al-mousawi@hotmail.com>
2021-10-25 11:39:47 +0200

    fixes added routes to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css'
+import AllRoutes from './Router/AllRoutes';
 
 function App() {
 
   return (
     <div className="App">
  
+      <AllRoutes>
+        {/* Navbar goes here. */}
+      </AllRoutes>
     </div>
   )
 }
