83b9bafaeda8034787a425c8f14350db53136f2c
oscargregorius <oscar.gregorius@gmail.com>
2021-10-25 11:07:46 +0200

    Notification Entity

---
+++ backend/src/main/java/com/server/backend/entities/Notification.java
@@ -0,0 +1,24 @@
+package com.server.backend.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Notification {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String message;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User userId;
+}
