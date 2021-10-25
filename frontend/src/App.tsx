8b0d2daceb44bf1ad1a57ff8263a59ffcf8f4f82
Ali94160 <ali.al-mousawi@hotmail.com>
2021-10-25 11:25:37 +0200

    removed default code.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,10 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Router>
-        <header className="App-header">
-        </header>
-        <main>
-        </main>
-      </Router>
+ 
     </div>
   )
 }
