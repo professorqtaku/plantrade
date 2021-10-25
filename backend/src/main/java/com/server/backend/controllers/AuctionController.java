49d2346948a968118962bb9c121b20dad850b4ea
bextex <r.sarvegard@gmail.com>
2021-10-25 10:45:41 +0200

    Add auctions entity, repo, service and controller

---
+++ backend/src/main/java/com/server/backend/controllers/AuctionController.java
@@ -0,0 +1,9 @@
+package com.server.backend.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/auctions")
+public class AuctionController {
+}
---
+++ backend/src/main/java/com/server/backend/services/AuctionService.java
@@ -0,0 +1,7 @@
+package com.server.backend.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuctionService {
+}
