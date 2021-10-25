49e2d3076ad72650ca2807c168e142fe7af2e95b
oscargregorius <oscar.gregorius@gmail.com>
2021-10-25 19:38:48 +0200

    Added a useEfffect to fetch all the auctions
    And importet the auctions via useContext.

--- frontend/src/Pages/AuctionPage/AuctionPage.tsx
+++ frontend/src/Pages/AuctionPage/AuctionPage.tsx
@@ -4,9 +4,20 @@ import {
   StyledContentWrapper,
 } from "./StyledAuctionPage";
 import AuctionCard from "../../Components/AuctionCard/AuctionCard";
-
+import { AuctionContext } from "../../Contexts/AuctionContext";
+import { useContext, useEffect } from "react";
 
 const AuctionPage = () => {
+  const { getAllAuctions, auctions } = useContext(AuctionContext);
+
+  useEffect(() => {
+    handleGetAuctions();
+  }, []);
+
+  const handleGetAuctions = async () => {
+    await getAllAuctions();
+  };
+
   return (
     <StyledWrapper>
       <StyledImg src="https://i.pinimg.com/564x/63/5e/b1/635eb177eef29242e96352f1206298da.jpg" />
