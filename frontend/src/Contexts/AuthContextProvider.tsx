a017bf1ffa633bf04bf166f1eaf33c7819e41f09
Ali94160 <ali.al-mousawi@hotmail.com>
2021-10-25 15:24:13 +0200

    Register form + submit + AuthContext fix.

--- frontend/src/Contexts/AuthContextProvider.tsx
+++ frontend/src/Contexts/AuthContextProvider.tsx
@@ -4,14 +4,33 @@ interface Props {
   children?: JSX.Element
 }
 
+type User = {
+  username: string,
+  email: string,
+  password: string
+}
+
 export const AuthProvider: React.FC<Props> = ({children}) => {
 
   const AuthContext = createContext<any>(null);
 
+  const [user, setUser] = useState({});
+  
 
+  const registerUser = async (user: User) => {
+    let res = await fetch("/api/register", {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' }
+    },
+    )
+    let newUser = await res.json();
+    setUser(newUser);
+    console.log(newUser , 'new User?')
+    
+  }
 
   const values = {
-    
+    registerUser
   }
 
   return (
--- frontend/src/Pages/RegisterPage/RegisterPage.tsx
+++ frontend/src/Pages/RegisterPage/RegisterPage.tsx
@@ -2,9 +2,20 @@
 import { StyledWrapper } from "./StyledRegisterPage";
 
 const RegisterPage = () => {
+
+  const handleRegister = (e: React.FormEvent) => {
+    e.preventDefault();
+ 
+    // Create user-object and call on the register func from AuthContext.
+  }
+
   return (
     <StyledWrapper>
       <h3>Register Page</h3>
+
+      <form onSubmit={(e) => handleRegister(e)}>
+
+      </form>
     </StyledWrapper>
   );
 }
