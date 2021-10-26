da50b5589e79b6e3b738b7a1078994898cb69f42
postila <isabella.postila@hotmail.com>
2021-10-26 11:35:14 +0200

    Added ResponseEntity status when creating bid

--- backend/src/main/java/com/server/backend/controllers/BidController.java
+++ backend/src/main/java/com/server/backend/controllers/BidController.java
@@ -3,9 +3,11 @@ package com.server.backend.controllers;
 import com.server.backend.entities.Bid;
 import com.server.backend.services.BidService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -15,8 +17,15 @@ public class BidController {
   private BidService bidService;
 
   @PostMapping("/auctions/{id}")
-  public Bid createBid(@RequestBody Map values){
-    return bidService.createBid(values);
+  public ResponseEntity<Bid> createBid(@RequestBody Map values) {
+    Bid bid = bidService.createBid(values);
+    
+    if (bid != null) {
+      return ResponseEntity.ok(bid);
+    } else {
+      return ResponseEntity.badRequest().build();
+    }
+
   }
 
 }
