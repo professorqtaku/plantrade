33f5a1a4c5ad14d6f7ec312d1b2b2bc4bba08e0f
oscargregorius <oscar.gregorius@gmail.com>
2021-10-26 15:53:30 +0200

    Added logic for min and max date.

--- frontend/src/Components/AuctionDatePicker/AuctionDatePicker.tsx
+++ frontend/src/Components/AuctionDatePicker/AuctionDatePicker.tsx
@@ -1,14 +1,23 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
+import { StyledDatePicker } from "./StyledAuctionDatePicket";
 
 import "react-datepicker/dist/react-datepicker.css";
 
 const AuctionDatePicker = () => {
   const [startDate, setStartDate] = useState(new Date());
+  let oneMonth = new Date();
+  oneMonth.setDate(oneMonth.getDate() + 30);
+
+  let oneDay = new Date();
+  oneDay.setDate(oneDay.getDate() + 1);
+
   return (
-    <DatePicker
+    <StyledDatePicker
       selected={startDate}
       onChange={(date: Date) => setStartDate(date)}
+      maxDate={oneMonth}
+      minDate={oneDay}
     />
   );
 };
---
+++ frontend/src/Components/AuctionDatePicker/StyledAuctionDatePicket.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import DatePicker from "react-datepicker";
+
+export const StyledDatePicker = styled(DatePicker)`
+  width: 85vw;
+  padding: 0.5rem;
+  outline: none;
+`;
