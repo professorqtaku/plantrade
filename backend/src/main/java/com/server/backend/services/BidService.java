54e2acc0af3d6e8e870f809c86d85a54de9cc65f
postila <isabella.postila@hotmail.com>
2021-10-26 11:36:14 +0200

    Validate so a user that is logged in only can create a bid

--- backend/src/main/java/com/server/backend/services/BidService.java
+++ backend/src/main/java/com/server/backend/services/BidService.java
@@ -7,6 +7,7 @@ import com.server.backend.repositories.AuctionRepository;
 import com.server.backend.repositories.BidRepository;
 import com.server.backend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -29,25 +30,27 @@ public class BidService {
     Auction currentAuction = auctionRepository.findById((long) (int) values.get("auctionId")).get();
 
     int index = currentAuction.getBids().size();
-    double oldPrice = currentAuction.getBids().get(index - 1).getPrice();
+    double latestPrice = currentAuction.getBids().get(index - 1).getPrice();
 
-    if(oldPrice < (double) (int) values.get("price") || currentAuction.getBids().isEmpty()){
-      try{
-        Bid bid = Bid.builder()
-                .user(currentUser)
-                .auction(currentAuction)
-                .price((int) values.get("price"))
-                .createdDate((long) values.get("createdDate"))
-                .build();
+    if(currentUser.getUsername().equals(SecurityContextHolder.getContext().getAuthentication().getName())){
+      if(latestPrice < (double) (int) values.get("price") || currentAuction.getBids().isEmpty()){
+        try{
+          Bid bid = Bid.builder()
+                  .user(currentUser)
+                  .auction(currentAuction)
+                  .price((int) values.get("price"))
+                  .createdDate((long) values.get("createdDate"))
+                  .build();
 
-        currentAuction.addBid(bid);
+          currentAuction.addBid(bid);
 
-        return bidRepository.save(bid);
-      } catch(Exception e) {
-        e.printStackTrace();
+          return bidRepository.save(bid);
+        } catch(Exception e) {
+          e.printStackTrace();
+        }
       }
+      return null;
     }
-
     return null;
   }
 }
