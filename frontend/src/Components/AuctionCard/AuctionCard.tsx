15078ca5591492bc3e06753874dc3af05814a540
oscargregorius <oscar.gregorius@gmail.com>
2021-10-26 14:22:01 +0200

    Hotfix

--- frontend/src/Components/AuctionCard/AuctionCard.tsx
+++ frontend/src/Components/AuctionCard/AuctionCard.tsx
@@ -20,7 +20,6 @@ const AuctionCard = ({ auction, fetchAuctions }: Props) => {
   const ONE_DAY_IN_MILLIS = 86400000;
   const ONE_HOUR_IN_MILLIS = 3600000;
   const ONE_MINUTE_IN_MILLIS = 60000;
-  const ONE_SECOND_IN_MILLIS = 6000;
 
   const [daysLeft, setDaysLeft] = useState<number | null>(null);
   const [differenceInMillis, setDifferenceInMillis] = useState(0);
