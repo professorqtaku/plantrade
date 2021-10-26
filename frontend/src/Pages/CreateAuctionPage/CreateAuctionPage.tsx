8c25f32e917e143de22cb873e98cad8c3795e272
oscargregorius <oscar.gregorius@gmail.com>
2021-10-26 15:04:00 +0200

    Added all the fields, and created the handleAddAuction function

--- frontend/src/Pages/CreateAuctionPage/CreateAuctionPage.tsx
+++ frontend/src/Pages/CreateAuctionPage/CreateAuctionPage.tsx
@@ -1,11 +1,29 @@
-import {StyledWrapper} from './StyledCreateAuctionPage'
+import {
+  StyledWrapper,
+  StyledForm,
+  StyledInput,
+  StyledTitle,
+  StyledButton,
+} from "./StyledCreateAuctionPage";
 
 const CreateAuctionPage = () => {
+
+  const handleAddAuction = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();    
+  }
+
   return (
     <StyledWrapper>
-      Create auction..
-    </StyledWrapper>  
-  )
-}
+      <StyledTitle>Skapa auktion</StyledTitle>
+      <StyledForm onSubmit={handleAddAuction}>
+        <StyledInput placeholder="Titel" />
+        <StyledInput placeholder="Beskrivning" />
+        <StyledInput placeholder="Start pris" />
+        <StyledInput placeholder="Slut datum" />
+        <StyledButton>Skapa auktion</StyledButton>
+      </StyledForm>
+    </StyledWrapper>
+  );
+};
 
 export default CreateAuctionPage;
--- frontend/src/Pages/CreateAuctionPage/StyledCreateAuctionPage.ts
+++ frontend/src/Pages/CreateAuctionPage/StyledCreateAuctionPage.ts
@@ -1,5 +1,32 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export const StyledWrapper = styled.div`
+export const StyledWrapper = styled.div``;
 
-`
+export const StyledTitle = styled.p`
+  font-size: 2rem;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  text-align: center;
+`;
+
+export const StyledForm = styled.form`
+  width: 90%;
+  margin: 1rem auto;
+  display: grid;
+  grid-gap: 1rem;
+`;
+
+export const StyledInput = styled.input`
+  padding: 0.5rem;
+  outline: none;
+`;
+
+export const StyledButton = styled.button`
+  background: #619463;
+  color: white;
+  border: none;
+  text-transform: uppercase;
+  font-size: 1rem;
+  letter-spacing: 3px;
+  padding: 0.5rem;
+`;
