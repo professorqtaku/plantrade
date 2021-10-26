0164ebe2ae1ec2d21e4073d5be8876cee2a578a7
oscargregorius <oscar.gregorius@gmail.com>
2021-10-26 16:20:03 +0200

    Changed the startdate

--- frontend/src/Components/AuctionDatePicker/AuctionDatePicker.tsx
+++ frontend/src/Components/AuctionDatePicker/AuctionDatePicker.tsx
@@ -5,12 +5,12 @@ import { StyledDatePicker } from "./StyledAuctionDatePicket";
 import "react-datepicker/dist/react-datepicker.css";
 
 const AuctionDatePicker = () => {
-  const [startDate, setStartDate] = useState(new Date());
   let oneMonth = new Date();
   oneMonth.setDate(oneMonth.getDate() + 30);
 
   let oneDay = new Date();
   oneDay.setDate(oneDay.getDate() + 1);
+  const [startDate, setStartDate] = useState(oneDay);
 
   return (
     <StyledDatePicker
