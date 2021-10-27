026a83af2a3237258354b280440aa94a7c40ad97
oscargregorius <oscar.gregorius@gmail.com>
2021-10-27 08:08:33 +0200

    Passed in state to the Select component to update the state on change

--- frontend/src/Components/SelectBar/SelectBar.tsx
+++ frontend/src/Components/SelectBar/SelectBar.tsx
@@ -18,50 +18,55 @@ const MenuProps = {
   },
 };
 
-const names = [
-  "Oliver Hansen",
-  "Van Henry",
-  "April Tucker",
-  "Ralph Hubbard",
-  "Omar Alexander",
-  "Carlos Abbott",
-  "Miriam Wagner",
-  "Bradley Wilkerson",
-  "Virginia Andrews",
-  "Kelly Snyder",
+const values = [
+  "Kategori 1",
+  "Kategori 2",
+  "Kategori 3",
+  "Kategori 4",
+  "Kategori 5",
+  "Kategori 6",
+  "Kategori 7",
+  "Kategori 8",
+  "Kategori 9",
+  "Kategori 10",
 ];
 
-const SelectBar = () => {
-  const [personName, setPersonName] = React.useState<string[]>([]);
+interface Props {
+  setCategoriesToUse: React.Dispatch<
+    React.SetStateAction<String[] | null>
+  >;
+  categoriesToUse: String[] | null;
+}
 
-  const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+const SelectBar = ({ setCategoriesToUse, categoriesToUse }: Props) => {
+  const [categories, setCategories] = React.useState<string[]>([]);
+
+  const handleChange = (event: SelectChangeEvent<typeof categories>) => {
     const {
       target: { value },
     } = event;
-    setPersonName(
-      // On autofill we get a the stringified value.
-      typeof value === "string" ? value.split(",") : value
-    );
+    setCategories(typeof value === "string" ? value.split(",") : value);
+    setCategoriesToUse(categories);
   };
 
   return (
     <div>
       <FormControl sx={{ m: 1, width: 300 }}>
-        <InputLabel id="demo-multiple-checkbox-label">Tag</InputLabel>
+        <InputLabel id="demo-multiple-checkbox-label">Kategorier</InputLabel>
         <Select
           labelId="demo-multiple-checkbox-label"
           id="demo-multiple-checkbox"
           multiple
-          value={personName}
+          value={categories}
           onChange={handleChange}
           input={<OutlinedInput label="Tag" />}
           renderValue={(selected) => selected.join(", ")}
           MenuProps={MenuProps}
         >
-          {names.map((name) => (
-            <MenuItem key={name} value={name}>
-              <Checkbox checked={personName.indexOf(name) > -1} />
-              <ListItemText primary={name} />
+          {values.map((category) => (
+            <MenuItem key={category} value={category}>
+              <Checkbox checked={categories.indexOf(category) > -1} />
+              <ListItemText primary={category} />
             </MenuItem>
           ))}
         </Select>
--- frontend/src/Pages/CreateAuctionPage/CreateAuctionPage.tsx
+++ frontend/src/Pages/CreateAuctionPage/CreateAuctionPage.tsx
@@ -6,15 +6,16 @@ import {
   StyledButton,
 } from "./StyledCreateAuctionPage";
 import AuctionDatePicker from "../../Components/AuctionDatePicker/AuctionDatePicker";
+import SelectBar from "../../Components/SelectBar/SelectBar";
 import { useState } from "react";
 
 const CreateAuctionPage = () => {
-
   const [endDate, setEndDate] = useState(0);
+  const [categoriesToUse, setCategoriesToUse] = useState<String[] | null>(null);
 
   const handleAddAuction = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();    
-  }
+    e.preventDefault();
+  };
 
   return (
     <StyledWrapper>
@@ -23,7 +24,11 @@ const CreateAuctionPage = () => {
         <StyledInput placeholder="Titel" />
         <StyledInput placeholder="Beskrivning" />
         <StyledInput placeholder="Start pris" />
-        <AuctionDatePicker endDate={setEndDate}/>
+        <AuctionDatePicker endDate={setEndDate} />
+        <SelectBar
+          setCategoriesToUse={setCategoriesToUse}
+          categoriesToUse={categoriesToUse}
+        />
         <StyledButton>Skapa auktion</StyledButton>
       </StyledForm>
     </StyledWrapper>
