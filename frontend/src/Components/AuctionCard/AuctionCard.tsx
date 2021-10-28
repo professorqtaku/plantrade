a71cf2d3812b8d780373fd7aebdd70d9e6c3b8a8
oscargregorius <oscar.gregorius@gmail.com>
2021-10-28 15:58:13 +0200

    Clean up

--- frontend/src/Components/AuctionCard/AuctionCard.tsx
+++ frontend/src/Components/AuctionCard/AuctionCard.tsx
@@ -19,9 +19,6 @@ interface Props {
 
 const AuctionCard = ({ auction, fetchAuctions }: Props) => {
   const ONE_DAY_IN_MILLIS = 86400000;
-  const ONE_HOUR_IN_MILLIS = 3600000;
-  const ONE_MINUTE_IN_MILLIS = 60000;
-  const ONE_SECOND_IN_MILLIS = 1000;
 
   const [daysLeft, setDaysLeft] = useState<number | null>(null);
   const [differenceInMillis, setDifferenceInMillis] = useState(0);
