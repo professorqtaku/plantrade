44e11efa76e3df1686a5b962ecfc4b1eab97c664
Qiling <professorqtaku@gmail.com>
2021-10-28 12:30:29 +0200

    added category specification

--- backend/src/main/java/com/server/backend/specifications/AuctionSpecification.java
+++ backend/src/main/java/com/server/backend/specifications/AuctionSpecification.java
@@ -1,10 +1,14 @@
 package com.server.backend.specifications;
 
 import com.server.backend.entities.Auction;
+import com.server.backend.entities.Category;
 import com.server.backend.entities.Status;
 import org.springframework.data.jpa.domain.Specification;
 
 import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+import java.util.List;
 
 public class AuctionSpecification {
     public static Specification<Auction> titleContains(String searchWord) {
@@ -16,8 +20,16 @@ public class AuctionSpecification {
 
     public static Specification<Auction> hasStatus(Status status) {
         return (root, query, builder) -> {
-            Expression<String> statusExpression =root.get("status");
+            Expression<String> statusExpression = root.get("status");
             return builder.equal(statusExpression, status);
+        };
+    }
+
+    public static Specification<Auction> containsCategoryWithName(String name) {
+        return (root, query, builder) -> {
+            Join<Object, Object> categoryJoin = root.join("categories", JoinType.INNER);
+            Expression<String> categoryExpression = categoryJoin.get("name");
+            return builder.equal(categoryExpression, name);
         };
     }
 }
