03792ab9987158d7385fab63959e502346d8bb8a
oscargregorius <oscar.gregorius@gmail.com>
2021-10-28 10:50:21 +0200

    Created the avatar and logout button.

--- frontend/src/Pages/MyPage/MyPage.tsx
+++ frontend/src/Pages/MyPage/MyPage.tsx
@@ -1,11 +1,29 @@
 import Avatar from "@mui/material/Avatar";
-import { StyledWrapper, StyledAvatar } from "./StyledMyPage";
+import {
+  StyledWrapper,
+  StyledAvatar,
+  StyledAvatarWrapper,
+  StyledEditWrapper,
+  StyledBoldText,
+  StyledText,
+} from "./StyledMyPage";
+import Button from "../../Components/Button/ButtonComp";
 import EditIcon from "@mui/icons-material/Edit";
 
 const MyPage = () => {
   return (
     <StyledWrapper>
-      <StyledAvatar>N</StyledAvatar>
+      <StyledAvatarWrapper>
+        <StyledAvatar>N</StyledAvatar>
+        <Button label="logga ut" />
+      </StyledAvatarWrapper>
+      <StyledEditWrapper>
+        <StyledText>
+          <StyledBoldText>Username: </StyledBoldText>
+          Oscar
+        </StyledText>
+        <EditIcon />
+      </StyledEditWrapper>
     </StyledWrapper>
   );
 };
--- frontend/src/Pages/MyPage/StyledMyPage.ts
+++ frontend/src/Pages/MyPage/StyledMyPage.ts
@@ -10,6 +10,23 @@ export const StyledAvatar = styled(Avatar)`
   background: #619463;
 `;
 
+export const StyledAvatarWrapper = styled.div`
+display: grid;
+grid-template-columns: 1fr 1fr;
+margin-bottom: 2rem;
+`
+
 export const StyledEditWrapper = styled.div`
 
+`
+
+export const StyledText = styled.span`
+  font-size: 1rem;
+  letter-spacing: 2px;
+`;
+
+export const StyledBoldText = styled.span`
+  font-size: 1rem;
+  font-weight: bold;
+  letter-spacing: 2px;
 `
