9f73513115e0e11d73121930da2f08316f29bdd0
Ali94160 <ali.al-mousawi@hotmail.com>
2021-10-29 13:21:46 +0200

    Created HomePage + compnent
    - Added image header
    - Added SearchForm (reused)
    - Added the design

--- frontend/src/Pages/HomePage/HomePage.tsx
+++ frontend/src/Pages/HomePage/HomePage.tsx
@@ -1,11 +1,28 @@
-
+import SearchField from "../../Components/Search/SearchForm/SearchForm"
+import SearchForm from "../../Components/Search/SearchForm/SearchForm"
+import {
+  StyledWrapper,
+  StyledSearchWrapper,
+  StyledImg,
+  StyledSearchFieldWrapper
+  
+ } from "./StyledHomePage";
 
 const HomePage = () => {
 
   return (
-    <div>
-        <h3>HomePage</h3>
-    </div>
+    <StyledWrapper>
+      <StyledSearchWrapper>
+        <StyledImg src="https://i.pinimg.com/564x/63/5e/b1/635eb177eef29242e96352f1206298da.jpg" alt="" />
+        <StyledSearchFieldWrapper>
+        <SearchForm />
+        </StyledSearchFieldWrapper>
+        </StyledSearchWrapper>
+
+      
+
+
+    </StyledWrapper>
   );
 }
 
--- frontend/src/Pages/HomePage/StyledHomePage.ts
+++ frontend/src/Pages/HomePage/StyledHomePage.ts
@@ -1,2 +1,28 @@
 import styled from "styled-components";
 
+export const StyledWrapper = styled.div`
+  max-height: 80vh;
+  display: grid;
+  grid-template-rows: 25vh 1fr;
+`;
+
+export const StyledSearchWrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  position: relative;
+`;
+
+export const StyledImg = styled.img`
+  width: 100%;
+  height: 75%;
+  object-fit: cover;
+`;
+
+export const StyledSearchFieldWrapper = styled.div`
+ width: 70%;
+  margin: 0 auto;
+  position: absolute;
+  top: 60%;
+  left: 12%;
+
+`;
