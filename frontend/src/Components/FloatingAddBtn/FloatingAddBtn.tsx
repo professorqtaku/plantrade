6674e497d9db0dd077cd6f92e80e36f2a847872f
oscargregorius <oscar.gregorius@gmail.com>
2021-11-01 11:22:41 +0100

    Created the floatingAddBtn component with content

---
+++ frontend/src/Components/FloatingAddBtn/FloatingAddBtn.tsx
@@ -0,0 +1,12 @@
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
+
+const FloatingAddBtn = () => {
+  return (
+    <Fab color="primary" aria-label="add">
+      <AddIcon />
+    </Fab>
+  );
+};
+
+export default FloatingAddBtn;
