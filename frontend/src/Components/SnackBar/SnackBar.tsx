3792bea8ec41110aadd52acd0b3db95445077a70
oscargregorius <oscar.gregorius@gmail.com>
2021-11-01 14:29:08 +0100

    Added content to the snackbar component

--- frontend/src/Components/SnackBar/SnackBar.tsx
+++ frontend/src/Components/SnackBar/SnackBar.tsx
@@ -1,9 +1,33 @@
-const SnackBar = () => {
-  return (
-    <div>
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert, { AlertProps } from "@mui/material/Alert";
+import { useState } from "react";
+import React from "react";
 
-    </div>
-  )
-}
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props,
+  ref
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+const SnackBar = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+      <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
+        This is a success message!
+      </Alert>
+    </Snackbar>
+  );
+};
 
 export default SnackBar;
