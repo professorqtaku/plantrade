3c7219fbfb6612879e6c76bd65b8386a1f9b4413
oscargregorius <oscar.gregorius@gmail.com>
2021-11-02 14:37:51 +0100

    Created a check if if the bid is 2 times higher than the current bid
    Co-Authored-By: Ali <72066097+ali94160@users.noreply.github.com>

--- frontend/src/Pages/AuctionDetailPage/AuctionDetailPage.tsx
+++ frontend/src/Pages/AuctionDetailPage/AuctionDetailPage.tsx
@@ -1,20 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { useHistory } from 'react-router-dom'
-import { useAuction } from '../../Contexts/AuctionContext';
-import { useBid } from '../../Contexts/BidContext';
-import { useAuth } from '../../Contexts/AuthContext';
+import { useHistory } from "react-router-dom";
+import { useAuction } from "../../Contexts/AuctionContext";
+import { useBid } from "../../Contexts/BidContext";
+import { useAuth } from "../../Contexts/AuthContext";
 import { Auction } from "../../Interfaces/Interfaces";
-import Grid from '@mui/material/Grid';
-import SkeletonCard from '../../Components/SkeletonCard/SkeletonCard';
-import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined';
-import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import CommentIcon from '@mui/icons-material/Comment';
-import InputField from '../../Components/InputField/InputField';
-import ImageCarousel from '../../Components/ImageCarousel/ImageCarousel';
-import ExpandableDescriptionBox from '../../Components/ExpandableDesc/ExpandableDescriptionBox';
-import ButtonComp from "../../Components/Button/ButtonComp"
+import Grid from "@mui/material/Grid";
+import SkeletonCard from "../../Components/SkeletonCard/SkeletonCard";
+import AccessTimeOutlinedIcon from "@mui/icons-material/AccessTimeOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import CommentIcon from "@mui/icons-material/Comment";
+import InputField from "../../Components/InputField/InputField";
+import ImageCarousel from "../../Components/ImageCarousel/ImageCarousel";
+import ExpandableDescriptionBox from "../../Components/ExpandableDesc/ExpandableDescriptionBox";
+import ButtonComp from "../../Components/Button/ButtonComp";
 import {
   StyledWrapper,
   StyledPrice,
@@ -28,7 +28,7 @@ import {
   StyledDate,
   StyledAccessTimeOutlinedIcon,
   StyledCalendarTodayOutlinedIcon,
-  StyledForm
+  StyledForm,
 } from "./StyledAuctionDetailPage";
 
 const AuctionDetailPage = () => {
@@ -38,65 +38,71 @@ const AuctionDetailPage = () => {
   const { getAuctionById } = useAuction();
   const { createBid } = useBid();
   const { whoAmI } = useAuth();
-  
+
   const [auction, setAuction] = useState<Auction | undefined>();
-  const [bid, setBid] = useState<string>('');
+  const [bid, setBid] = useState<string>("");
   const [currentBid, setCurrentBid] = useState<number>(0);
   const [endDate, setEndDate] = useState<string>();
   const [endTime, setEndTime] = useState<string>();
-  const [bidText, setBidText] = useState<string>('');
+  const [bidText, setBidText] = useState<string>("");
   const [isHost, setIsHost] = useState<boolean>(false);
+  const [isOverPrice, setIsOverPrice] = useState(false);
 
   useEffect(() => {
     handleGetAuctionById();
-  }, [])
-
+  }, []);
 
   const handleGetAuctionById = async () => {
     const res = await getAuctionById(id);
-    setAuction(res)
-    setEndDate(new Date(res.endDate).toLocaleDateString('sv-SV'))
-    setEndTime(new Date(res.endDate).toLocaleTimeString('sv-SV'))
+    setAuction(res);
+    setEndDate(new Date(res.endDate).toLocaleDateString("sv-SV"));
+    setEndTime(new Date(res.endDate).toLocaleTimeString("sv-SV"));
 
     if (whoAmI && whoAmI.id == res.host.id) {
       setIsHost(true);
     }
-    
+
     if (res.bids.length) {
-      setCurrentBid(res.bids.pop(res.bids.length - 1).price)
-      setBidText('Högsta budet')
+      setCurrentBid(res.bids.pop(res.bids.length - 1).price);
+      setBidText("Högsta budet");
     } else {
-      setCurrentBid(res.startPrice)
-      setBidText('Startpris')
+      setCurrentBid(res.startPrice);
+      setBidText("Startpris");
     }
-  }
+  };
 
   const handleBid = async (e: any) => {
-    e.preventDefault()
+    e.preventDefault();
     if (whoAmI == null) return;
+    if (parseInt(bid) > currentBid * 2) {
+      setIsOverPrice(true);
+      return;
+    }
 
     const newBid = {
       userId: whoAmI.id,
       auctionId: auction?.id,
       price: parseInt(bid),
-      createdDate: Date.now()
-    }
+      createdDate: Date.now(),
+    };
 
     await createBid(newBid);
     //rerender the new currently highest bid
     handleGetAuctionById();
-    setBid('')
-  }
+    setBid("");
+  };
 
   const handleChat = () => {
-    console.log('I want to chat with the seller')
-  }
+    console.log("I want to chat with the seller");
+  };
 
   return (
     <StyledWrapper>
-      {!auction ? <SkeletonCard /> :
-        (<>
-          <StyledBackBtn onClick={() => history.push('/auctions')}>
+      {!auction ? (
+        <SkeletonCard />
+      ) : (
+        <>
+          <StyledBackBtn onClick={() => history.push("/auctions")}>
             <ArrowBackIosIcon />
             Tillbaka
           </StyledBackBtn>
@@ -111,19 +117,23 @@ const AuctionDetailPage = () => {
             </Grid>
             <Grid item xs={4} md={4}>
               <StyledChat onClick={handleChat}>
-                  <StyledChatIcon>
-                    <CommentIcon />
-                  </StyledChatIcon>
-                </StyledChat>
+                <StyledChatIcon>
+                  <CommentIcon />
+                </StyledChatIcon>
+              </StyledChat>
             </Grid>
             <Grid item xs={8} md={8}>
               <StyledUnderTitle>Sluttid</StyledUnderTitle>
               <StyledDate>
-                <StyledCalendarTodayOutlinedIcon><CalendarTodayOutlinedIcon /></StyledCalendarTodayOutlinedIcon>
+                <StyledCalendarTodayOutlinedIcon>
+                  <CalendarTodayOutlinedIcon />
+                </StyledCalendarTodayOutlinedIcon>
                 {endDate}
               </StyledDate>
               <StyledDate>
-                <StyledAccessTimeOutlinedIcon><AccessTimeOutlinedIcon/></StyledAccessTimeOutlinedIcon>
+                <StyledAccessTimeOutlinedIcon>
+                  <AccessTimeOutlinedIcon />
+                </StyledAccessTimeOutlinedIcon>
                 {endTime}
               </StyledDate>
             </Grid>
@@ -133,18 +143,27 @@ const AuctionDetailPage = () => {
             </Grid>
             <Grid item xs={12} md={6}>
               <StyledUnderTitle>Beskrivning</StyledUnderTitle>
-              <ExpandableDescriptionBox auctionDescription={auction.description} />
+              <ExpandableDescriptionBox
+                auctionDescription={auction.description}
+              />
             </Grid>
-            <Grid item xs={12} md={12} >
+            <Grid item xs={12} md={12}>
               #tags
             </Grid>
-            <StyledForm >
-              <InputField label="Lägg ett bud" type="number" value={bid} updateState={setBid} />
+            <StyledForm>
+              <InputField
+                label="Lägg ett bud"
+                type="number"
+                value={bid}
+                updateState={setBid}
+              />
               <ButtonComp label="Buda" callback={handleBid} disabled={isHost} />
             </StyledForm>
-          </Grid></>)}
+          </Grid>
+        </>
+      )}
     </StyledWrapper>
   );
-}
+};
 
 export default AuctionDetailPage;
