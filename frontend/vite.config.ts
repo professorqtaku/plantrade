2e520440521024b189a23d485ca5f81851ef5c01
Qiling <professorqtaku@gmail.com>
2021-11-03 09:59:47 +0100

    update db from demo 1
    Co-Authored-By: Ali <72066097+ali94160@users.noreply.github.com>
    Co-Authored-By: Alexander S. <70532114+autumnjava@users.noreply.github.com>
    Co-Authored-By: bextex <72066237+bextex@users.noreply.github.com>
    Co-Authored-By: oscargregorius <54839993+oscargregorius@users.noreply.github.com>
    Co-Authored-By: postila <72066238+postila@users.noreply.github.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     proxy: {
       '/api': 'http://localhost:4000',
       '/rest': 'http://localhost:4000',
