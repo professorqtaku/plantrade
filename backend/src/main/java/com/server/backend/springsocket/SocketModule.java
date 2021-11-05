8ecc53cf42ea3b2198a730dc34b9fb8cbb788ab1
oscargregorius <oscar.gregorius@gmail.com>
2021-11-05 11:39:53 +0100

    Added the socket package/class with content.
    Co-Authored-By: Ali <72066097+ali94160@users.noreply.github.com>

---
+++ backend/src/main/java/com/server/backend/springsocket/SocketModule.java
@@ -0,0 +1,92 @@
+package com.server.backend.springsocket;
+
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.listener.ConnectListener;
+import com.corundumstudio.socketio.listener.DataListener;
+import com.corundumstudio.socketio.listener.DisconnectListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SocketModule {
+    private SocketIOServer server;
+
+    public SocketModule() {
+        // prevent accidental starting multiple servers
+        if(server != null) return;
+
+        Configuration config = new Configuration();
+        // update host in production
+        config.setHostname("0.0.0.0"); // default is "localhost"
+        config.setPort(9092); // could be any port
+        server = new SocketIOServer(config);
+
+        // add connection event listeners
+        server.addConnectListener(onConnected());
+        server.addDisconnectListener(onDisconnected());
+
+        // add custom message event listeners (ChatMessage gets stringified/parsed automatically)
+        server.addEventListener("chat", ChatMessage.class, onChatReceived());
+
+        // add room support (the data is the room name)
+        server.addEventListener("join", String.class, onJoinRoom());
+        server.addEventListener("leave", String.class, onLeaveRoom());
+
+        // start socket.io server
+        server.start();
+    }
+
+    // method to emit message to all connected clients
+    public void emit(String event, Object data) {
+        server.getBroadcastOperations().sendEvent(event, data);
+    }
+
+    // method to emit message to all connected clients in a room
+    public void emitToRoom(String room, String event, Object data) {
+        server.getRoomOperations(room).sendEvent(event, data);
+    }
+
+    private DataListener<ChatMessage> onChatReceived() {
+        return (client, data, ackSender) -> {
+            System.out.printf("Client[%s] - Received chat message '%s'\n", client.getSessionId().toString(), data);
+
+            // send message to all connected clients
+            emit("chat", data);
+        };
+    }
+
+    private DataListener<String> onJoinRoom() {
+        return (client, roomName, ackSender) -> {
+            System.out.printf("Client[%s] - Joined room '%s'\n", client.getSessionId().toString(), roomName);
+            // add client to room
+            client.joinRoom(roomName);
+
+            // message room that client connected
+            emitToRoom(roomName, "join", "Client joined room: " + roomName);
+        };
+    }
+
+    private DataListener<String> onLeaveRoom() {
+        return (client, roomName, ackSender) -> {
+            System.out.printf("Client[%s] - Left room '%s'\n", client.getSessionId().toString(), roomName);
+            // remove client to room
+            client.leaveRoom(roomName);
+
+            // message room that client disconnected
+            emitToRoom(roomName, "leave", "Client left room: " + roomName);
+        };
+    }
+
+    private ConnectListener onConnected() {
+        return client -> {
+            System.out.printf("Client[%s] - Connected to chat module.\n", client.getSessionId().toString());
+        };
+    }
+
+    private DisconnectListener onDisconnected() {
+        return client -> {
+            System.out.printf("Client[%s] - Disconnected from chat module.\n", client.getSessionId().toString());
+        };
+    }
+
+}
