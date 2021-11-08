1f42fd7e606a6dff966115833769b9a79e4c4a45
autumnjava <creizy@gmail.com>
2021-11-08 16:16:17 +0100

    lil padding

--- frontend/src/Components/AuctionDetailsPageCategories/StyledAuctionDetailsPageCategories.ts
+++ frontend/src/Components/AuctionDetailsPageCategories/StyledAuctionDetailsPageCategories.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import {Chip} from '@mui/material'
 
 export const StyledChip = styled(Chip)`
+  margin: 2px;
   color: white;
   background-color: var(--light-green);
 `;
