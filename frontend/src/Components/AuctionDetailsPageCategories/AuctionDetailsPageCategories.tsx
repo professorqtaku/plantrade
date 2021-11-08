b7e0c582ac0824e4e6f2895fe6a62fd0e0b4a52c
autumnjava <creizy@gmail.com>
2021-11-08 16:16:46 +0100

    show more / show less

--- frontend/src/Components/AuctionDetailsPageCategories/AuctionDetailsPageCategories.tsx
+++ frontend/src/Components/AuctionDetailsPageCategories/AuctionDetailsPageCategories.tsx
@@ -1,24 +1,40 @@
+import { useState } from "react";
 import Grid from '@mui/material/Grid';
-import {  StyledChip } from "./StyledAuctionDetailsPageCategories";
+import ButtonComp from "../Button/ButtonComp";
 import { Category } from "../../Interfaces/Interfaces";
 interface Props {
   categories: Category[];
   auctionId: Number;
 }
+import {  StyledChip, StyledAddIcon } from "./StyledAuctionDetailsPageCategories";
+import AddIcon from "@mui/icons-material/Add";
+import { borderRadius } from "@mui/system";
+
 
 const AuctionDetailsPageCategories = ({ categories, auctionId }: Props) => {
+  const [toggleShowMore, setToggleShowMore ] = useState(false);
+  const size = 2;
+  const categoriesLimited = categories.slice(0, size)
+
+  const mapThroughAndRender = (_categories: Category[]) => {
+    return _categories.map(category => (
+      <StyledChip
+       key={`${auctionId}-${category.name}`}
+       label={`#${category.name}`}
+       />
+       ))
+  }
 
   return (
+    <>
   <Grid item xs={12} md={12}>
-    {categories
-    ? categories.map(category => (
-    <StyledChip
-     key={`${auctionId}-${category.name}`}
-     label={`#${category.name}`}
-     />
-     ))
-     : null}
+   {!toggleShowMore ? mapThroughAndRender(categoriesLimited) :
+     mapThroughAndRender(categories)
+     }
+     <ButtonComp label={!toggleShowMore ? "Visa mer" : "Dölj"} callback={() => setToggleShowMore(!toggleShowMore)} />
+     
      </Grid>
+     </>
   )
 }
 
