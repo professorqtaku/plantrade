e58d88a701bd20f51b402ac1d547566d8ed52eb8
oscargregorius <oscar.gregorius@gmail.com>
2021-11-09 12:04:43 +0100

    Made the header dynamic depending on the content
    Co-Authored-By: Ali <72066097+ali94160@users.noreply.github.com>

--- frontend/src/Components/DrawerCollapse/DrawerHeader/DrawerHeader.tsx
+++ frontend/src/Components/DrawerCollapse/DrawerHeader/DrawerHeader.tsx
@@ -1,16 +1,31 @@
-import { StyledHeader } from "./StyledDrawerHeader";
+import {
+  StyledHeader,
+  StyledExpandIcon,
+  StyledText,
+} from "./StyledDrawerHeader";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import {useDrawer} from '../../../Contexts/DrawerContext'
 
 interface Props {
   toggle: Function
 }
 
-const DrawerHeader = ({toggle} : Props) => {
+const DrawerHeader = ({ toggle }: Props) => {
+  const { showChatRoom, content, setShowChatRoom } = useDrawer();
+
   return (
     <StyledHeader>
-      <div onClick={() => toggle()}>
-        <ExpandMoreIcon />
-      </div>
+      {!showChatRoom ? (
+        <StyledText>Meddelanden</StyledText>
+      ) : (
+        <StyledText onClick={() => setShowChatRoom(false)}>Tillbaka</StyledText>
+      )}
+      <StyledExpandIcon onClick={() => toggle()} />
+      {!showChatRoom ? (
+        <StyledText>Antal: {content.length}</StyledText>
+      ) : (
+        <StyledText>Name on auction</StyledText>
+      )}
     </StyledHeader>
   );
 };
--- frontend/src/Components/DrawerCollapse/DrawerHeader/StyledDrawerHeader.ts
+++ frontend/src/Components/DrawerCollapse/DrawerHeader/StyledDrawerHeader.ts
@@ -1,7 +1,18 @@
 import styled from "styled-components";
-import { Collapse } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 export const StyledHeader = styled.div`
   color: black;
-  text-align: center;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+`;
+
+export const StyledExpandIcon = styled(ExpandMoreIcon)`
+  align-self: center;
+  justify-self: center;
+`;
+
+export const StyledText = styled.p`
+  align-self: center;
+  
 `;
