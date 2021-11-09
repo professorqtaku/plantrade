eb195a677e14711e452c51b462202e4807d2c49a
autumnjava <creizy@gmail.com>
2021-11-09 11:08:12 +0100

    change

--- frontend/src/Components/ImageCarousel/ImageCarousel.tsx
+++ frontend/src/Components/ImageCarousel/ImageCarousel.tsx
@@ -7,35 +7,6 @@ import { StyledImage, StyledPaper } from '../../Pages/AuctionDetailPage/StyledAu
 
 
 function ImageCarousel(images:any) {
-
-
-  // REMOVE WHEN IMAGES COME FROM AUCTION - ONLY MOCKUP
-  images = [
-  {
-    label: 'San Francisco – Oakland Bay Bridge, United States',
-    path:
-      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
-  },
-  {
-    label: 'Bird',
-    path:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
-  },
-  {
-    label: 'Bali, Indonesia',
-    path:
-      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80',
-  },
-  {
-    label: 'Goč, Serbia',
-    path:
-      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
-  },
-  ];
-  
-  console.log('this is images', images);
-  
-  
   return (
     <Carousel
       NextIcon={<NavigateNextIcon />}
