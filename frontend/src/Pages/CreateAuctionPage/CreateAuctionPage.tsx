d11c736090b6a7bb795badac0dd076387eda0a51
autumnjava <creizy@gmail.com>
2021-11-09 16:47:08 +0100

    fixed preview + errorMsg

--- frontend/src/Pages/CreateAuctionPage/CreateAuctionPage.tsx
+++ frontend/src/Pages/CreateAuctionPage/CreateAuctionPage.tsx
@@ -4,11 +4,13 @@ import {
   StyledTitle,
   StyledButton,
   StyledText,
+  StyledImage
 } from "./StyledCreateAuctionPage";
+import Grid from '@mui/material/Grid';
 import AuctionDatePicker from "../../Components/AuctionDatePicker/AuctionDatePicker";
 import SelectBar from "../../Components/SelectBar/SelectBar";
 import InputField from "../../Components/InputField/InputField";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { styled } from "@mui/material/styles";
 import Button from "@mui/material/Button";
 import { useAuction } from "../../Contexts/AuctionContext";
@@ -16,20 +18,19 @@ import { useHistory } from "react-router";
 import { Category } from "../AuctionPage/AuctionPage";
 import { useSocket } from "../../Contexts/SocketContext";
 
-
-
 const Input = styled("input")({
   display: "none",
 });
+
+const formData = new FormData();
 
 const CreateAuctionPage = () => {
   const { createAuction } = useAuction();
   const history = useHistory();
   const { socket } = useSocket();
 
-  // const [preview, setPreview] = useState('')
+  const [preview, setPreview] = useState<string[]>([]);
   // create a holder to store files
-  const formData = new FormData()
   const [title, setTitle] = useState<string>('');
   const [desc, setDesc] = useState<string>('');
   const [price, setPrice] = useState<number>(0);
@@ -43,34 +44,32 @@ const CreateAuctionPage = () => {
 
   const handleAddAuction = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-      if(formData.has('files')){
-        setErrorMsg(false);
-      } else {
-        setErrorMsg(true);
-        return;
-      }
+    setErrorMsg(formData.has('files') ? false : true)
 
-    const auction = {
-      title: title,
-      description: desc,
-      startPrice: price,
-      endDate: endDate,
+    if(formData.has('files')){
+      const auction = {
+        title: title,
+        description: desc,
+        startPrice: price,
+        endDate: endDate,
+      };
+
+      
+      if(auction.title && auction.description && auction.startPrice){
+        const res =  await createAuction(auction, categoriesToUse, formData);
+        setTitle('');
+        setDesc('');
+        setPrice(0);
+        setEndDate(inOneDay);
+        setCategoriesToUse([]);
+        formData.delete('files');
+
+        if (res.id) {
+          socket.emit("join", res.id);
+          history.push(`/auctions/${res.id}`)
+        }
     };
-    if(auction.title && auction.description && auction.startPrice){
-
-      const res =  await createAuction(auction, categoriesToUse, formData);
-      
-      setTitle('');
-      setDesc('');
-      setPrice(0);
-      setEndDate(inOneDay);
-      setCategoriesToUse([]);
-      
-      if (res.id) {
-        socket.emit("join", res.id);
-        history.push(`/auctions/${res.id}`)
-      }
-  };
+  }
 }
 
   const renderUploadFiles = () => (
@@ -91,15 +90,29 @@ const CreateAuctionPage = () => {
 
   async function onFileLoad(e: any) {
     const files = e.target.files;
+    let arr = [];
 
     // add files to formData
     for (let file of files) {
       formData.append('files', file, file.name);
+      const src = URL.createObjectURL(file);
+      arr.push(src)
     }
-
-    // clear input of files
+    setPreview(arr);
+    setErrorMsg(files.length ? false : true)
     e.target.value = '';
-  }
+    arr = [];
+}
+
+  const renderImagePreview = () => (
+  <Grid container>
+    <Grid item xs={12} md={12}>
+      {preview.map(img => (
+        <StyledImage key={img} src={img} alt="" />
+      ))}
+    </Grid>
+  </Grid>
+  )
 
   return (
     <StyledWrapper>
@@ -130,6 +143,7 @@ const CreateAuctionPage = () => {
         </label>
         <SelectBar setCategoriesToUse={setCategoriesToUse} />
         {renderUploadFiles()}
+        {renderImagePreview()}
         <StyledButton type="submit" variant="contained">
           {" "}
           Skapa auktion{" "}
