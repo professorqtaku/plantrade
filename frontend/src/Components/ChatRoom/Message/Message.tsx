2bde051ae600e189da0552e436d0a93c4ac82605
oscargregorius <oscar.gregorius@gmail.com>
2021-11-10 08:13:06 +0100

    Created a render method, the code is now more readable.
    Co-Authored-By: Ali <72066097+ali94160@users.noreply.github.com>

--- frontend/src/Components/ChatRoom/Message/Message.tsx
+++ frontend/src/Components/ChatRoom/Message/Message.tsx
@@ -8,17 +8,19 @@ import {
 import { useMessage } from "../../../Contexts/MessageContext";
 
 const Message = () => {
+  const renderMessageContent = (message: any) => (
+    <StyledMessageWrapper key={Math.random() * 100}>
+      <StyledMessage id={message.id}>
+        <StyledText>{message.text}</StyledText>
+      </StyledMessage>
+      <StyledDate id={message.id}>2020-10-12</StyledDate>
+    </StyledMessageWrapper>
+  );
+
   const { messages } = useMessage();
   return (
     <StyledWrapper>
-      {messages.map((message: any) => (
-        <StyledMessageWrapper>
-          <StyledMessage id={message.id}>
-            <StyledText>{message.text}</StyledText>
-          </StyledMessage>
-          <StyledDate id={message.id}>2020-10-12</StyledDate>
-        </StyledMessageWrapper>
-      ))}
+      {messages.map((message: any) => renderMessageContent(message))}
     </StyledWrapper>
   );
 };
