865db9fbb892b5f00fcdf9199d9b360c4d20fa43
oscargregorius <oscar.gregorius@gmail.com>
2021-11-10 11:55:10 +0100

    Prepared the form with a onSubmit and state.
    Co-Authored-By: Ali <72066097+ali94160@users.noreply.github.com>

--- frontend/src/Components/ChatRoom/TextInput/StyledTextInput.ts
+++ frontend/src/Components/ChatRoom/TextInput/StyledTextInput.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import SendIcon from "@mui/icons-material/Send";
 
-export const StyledWrapper = styled.div`
+export const StyledForm = styled.form`
   background: #f8f8f8;
   border-top: 1px solid lightgray;
   display: grid;
@@ -12,14 +12,28 @@ export const StyledWrapper = styled.div`
 export const StyledInput = styled.input`
   background: white;
   border: none;
-  padding: 0.5rem;
+  height: 1.5rem;
+  border-radius: 5px;
+  padding: 0.3rem;
   color: black;
   outline: none;
-  align-self: center;
+  align-self: start;
   margin-left: 0.7rem;
+  margin-top: 0.5rem;
   box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
+  @media (min-width: 768px) {
+    margin-top: 2rem;
+  }
 `;
 
 export const StyledSendIcon = styled(SendIcon)`
-  align-self: center;
+  align-self: start;
+  margin-top: 0.8rem;
+  @media (min-width: 768px) {
+    margin-top: 2.2rem;
+  }
 `;
+
+export const StyledButton = styled.button`
+  border: none;
+`
--- frontend/src/Components/ChatRoom/TextInput/TextInput.tsx
+++ frontend/src/Components/ChatRoom/TextInput/TextInput.tsx
@@ -1,12 +1,39 @@
-import { StyledWrapper, StyledInput, StyledSendIcon } from "./StyledTextInput";
+import { useState } from "react";
+import {
+  StyledForm,
+  StyledInput,
+  StyledSendIcon,
+  StyledButton,
+} from "./StyledTextInput";
 
 const TextInput = () => {
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const newMessage = {
+      message: message,
+    };
+    console.log(newMessage);
+
+    // Logic to send obj to backend
+    setMessage("");
+  };
+
   return (
-    <StyledWrapper>
-      <StyledInput placeholder="Skriv ditt meddelande här" />
-      <StyledSendIcon/>
-    </StyledWrapper>
-  )
-}
+    <div>
+      <StyledForm onSubmit={(e) => handleSubmit(e)}>
+        <StyledInput
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Skriv ditt meddelande här"
+        />
+        <StyledButton>
+          <StyledSendIcon />
+        </StyledButton>
+      </StyledForm>
+    </div>
+  );
+};
 
 export default TextInput;
