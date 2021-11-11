1ec170963d802b14a3982abefa37a6816067b0ef
oscargregorius <oscar.gregorius@gmail.com>
2021-11-11 09:12:02 +0100

    Added correct data and methods to the chat context.
    Co-Authored-By: Ali <72066097+ali94160@users.noreply.github.com>

--- frontend/src/Contexts/ChatContext.tsx
+++ frontend/src/Contexts/ChatContext.tsx
@@ -1,14 +1,41 @@
-import { createContext, FC, useContext } from "react";
+import { createContext, FC, useContext, useState } from "react";
 
 type Props = {
   children?: JSX.Element;
 };
+
 
 const ChatContext = createContext<any>(null);
 
 export const useChat = () => useContext(ChatContext);
 
 const ChatProvider = ({ children }: Props) => {
+
+  const [chats, setChats] = useState([]);
+
+  const getChatsByCurrentUser = async () => {
+    let res: Response = await fetch(`/api/chats`);
+    const chatResponse = await res.json();
+    if (chatResponse.status === 200) {
+      setChats(chatResponse);
+    }
+  }
+
+  const createChat = async (auctionId: number) => {
+    let res: Response = await fetch(`/api/chats`, {
+      method: "POST",
+      body: JSON.stringify(auctionId),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const chatResponse = await res.json();
+    if (chatResponse.status === 200) {
+      getChatsByCurrentUser();
+    }
+  };
+
+
   const values = {};
 
   return <ChatContext.Provider value={values}>{children}</ChatContext.Provider>;
--- frontend/src/Contexts/MessageContext.tsx
+++ frontend/src/Contexts/MessageContext.tsx
@@ -12,7 +12,7 @@ export const useMessage = () => useContext(MessageContext);
 const MessageProvider: FC<Props> = ({ children }: Props) => {
   const [messages, setMessages] = useState([]);
 
-  const getAllChatMsg = async (chatId: string) => {
+  const getAllChatMsg = async (chatId: number) => {
     let res: Response = await fetch(`/api/messages/${chatId}`);
     if (res.status === 200) {
       let messages = await res.json();
@@ -20,7 +20,7 @@ const MessageProvider: FC<Props> = ({ children }: Props) => {
     }
   };
 
-  const createMsg = async (chatId: string) => {
+  const createMsg = async (chatId: number) => {
     let res: Response = await fetch(`/api/messages/${chatId}`, {
       method: "POST",
     });
