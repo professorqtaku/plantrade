bfd72484a02a59e7abcba3424fe337ffd1d8cfec
Qiling <professorqtaku@gmail.com>
2021-11-11 14:39:01 +0100

    add colors

--- frontend/src/Components/SnackBar/SnackBar.tsx
+++ frontend/src/Components/SnackBar/SnackBar.tsx
@@ -1,14 +1,22 @@
 import React, { forwardRef, useCallback } from "react";
 import { useSnackbar, SnackbarContent } from "notistack";
-import { Typography, IconButton } from '@mui/material'
+import { Typography, IconButton } from "@mui/material";
 import CloseIcon from "@material-ui/icons/Close";
 import { StyledCard, StyledCardActions } from "./StyledSnackBar";
 import { Notification } from "../../Interfaces/Interfaces";
 
-
-const SnackMessage = forwardRef<HTMLDivElement,{ id: string | number; message: string | Notification }>((props, ref) => {
+const SnackMessage = forwardRef<
+  HTMLDivElement,
+  { id: string | number; message: string | Notification; variant: "success" | "error" | "warning" | "info" }
+>((props, ref) => {
   const { closeSnackbar } = useSnackbar();
-  
+
+const styles = {
+  success: "purple" ,
+  error: "blue" ,
+  warning: "green",
+  info: "yellow" ,
+};
 
   const handleDismiss = useCallback(() => {
     closeSnackbar(props.id);
@@ -16,13 +24,12 @@ const SnackMessage = forwardRef<HTMLDivElement,{ id: string | number; message: s
 
   return (
     <SnackbarContent ref={ref}>
-      <StyledCard>
+      <StyledCard colour={styles[variant]}>
         <StyledCardActions>
           <Typography variant="subtitle2">
             {typeof props.message === "object"
               ? props.message.message
-              : props.message
-            }
+              : props.message}
           </Typography>
           <div>
             <IconButton onClick={handleDismiss}>
