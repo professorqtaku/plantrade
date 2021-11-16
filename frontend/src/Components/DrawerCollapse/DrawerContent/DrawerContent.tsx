71fe76f4fbc6a6b07cc5301e5ba224ec96ff7283
Qiling <professorqtaku@gmail.com>
2021-11-16 08:58:49 +0100

    add placeholder

--- frontend/src/Components/DrawerCollapse/DrawerContent/DrawerContent.tsx
+++ frontend/src/Components/DrawerCollapse/DrawerContent/DrawerContent.tsx
@@ -7,7 +7,7 @@ import { useNav } from "../../../Contexts/NavigationContext";
 import NotificationDrawerItem from "../DrawerItem/NotificationDrawerItem/NotificationDrawerItem";
 
 const DrawerContent = () => {
-  const { message } = useNav();
+  const { message, notis } = useNav();
   const { chats } = useChat();
   const { notifications } = useNotification();
 
@@ -17,9 +17,10 @@ const DrawerContent = () => {
         ? chats.map((chat: Chat) => (
             <DrawerItem key={`chat-item-${chat.id}`} chat={chat} />
           ))
-        : notifications &&
-          notifications.length > 0 &&
-          notifications.map((notification: Notification) => (
+        : notis
+          && notifications
+          && notifications.length > 0
+          && notifications.map((notification: Notification) => (
             <NotificationDrawerItem
               key={`notification-item-${notification.id}`}
               notification={notification}
