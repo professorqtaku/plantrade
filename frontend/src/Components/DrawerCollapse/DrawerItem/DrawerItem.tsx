ad08c853132ccdd1d1f60e3d6c4b92cbe26d355d
Ali94160 <ali.al-mousawi@hotmail.com>
2021-11-18 14:14:47 +0100

    he reciver gets the badge of a missing msg.

--- frontend/src/Components/DrawerCollapse/DrawerItem/DrawerItem.tsx
+++ frontend/src/Components/DrawerCollapse/DrawerItem/DrawerItem.tsx
@@ -18,77 +18,105 @@ import {
 } from "react-swipeable-list";
 import "react-swipeable-list/dist/styles.css";
 import { useDrawer } from "../../../Contexts/DrawerContext";
-import { Chat } from "../../../Interfaces/Interfaces";
+import { Chat, Message } from "../../../Interfaces/Interfaces";
 import { useChat } from "../../../Contexts/ChatContext";
 import { useMessage } from "../../../Contexts/MessageContext";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useAuth } from "../../../Contexts/AuthContext";
 
 interface Props {
   chat: Chat;
 }
 
   const DrawerItem = ({ chat }: Props) => {
-  const { setShowChatRoom } = useDrawer();
-  const { setChatTitle, setChatId, getChatsByCurrentUser } = useChat();
-  const { messages, getAllChatMsg } = useMessage();
+    const { setShowChatRoom } = useDrawer();
+    const { setChatTitle, setChatId, getChatsByCurrentUser } = useChat();
+    const { messages, getAllChatMsg } = useMessage();
+    const { whoAmI } = useAuth();
+    const [counter, setCounter] = useState(0);
+    const { isRead } = useSocket();
 
-    
-  const handleShowMessageView = () => {
-    setChatId(chat.id)
-    setShowChatRoom(true);
-    setChatTitle(chat.auction.title);
-  }
+    const handleShowMessageView = () => {
+      setChatId(chat.id);
+      setShowChatRoom(true);
+      setChatTitle(chat.auction.title);
+    };
 
     useEffect(() => {
       getAllChatMsg(chat?.id);
       getChatsByCurrentUser();
     }, []);
-    
 
-  const trailingActions = () => (
-    <TrailingActions>
-      <SwipeAction
-        destructive={true}
-        onClick={() => console.info("swipe action triggered")}
-      >
-        <StyledDelete>
-          <StyledTrashCan />
-          <StyledDeleteText>Ta bort</StyledDeleteText>
-        </StyledDelete>
-      </SwipeAction>
-    </TrailingActions>
-  );
+    useEffect(() => {
+      badgeCounter(chat.messages, whoAmI.id);
+      console.log(whoAmI.id , 'whoID?')
+    },[setCounter]);
 
-  const renderMsgContent = (
-    <StyledInnerWrapper>
-      <StyledTitle>{chat.auction.title}</StyledTitle>
-      {chat?.messages && chat.messages.slice(-1).pop() && (
-        <StyledLastMsg>
-          <StyledName>
-             {chat.messages && chat.messages.slice(-1).pop()?.writer.username}:
-          </StyledName>
-          {chat.messages && chat.messages.slice(-1).pop()?.message}
-        </StyledLastMsg>
-      )}
-    </StyledInnerWrapper>
-  );
+    // chat?.messages[chat?.messages?.length - 1]
 
-  const renderBadge = (
-    <StyledBadge> 
-      <StyledAvatar>{chat.receiver.username.charAt(0).toLocaleUpperCase()}</StyledAvatar>
-    </StyledBadge>
-  );
+    const badgeCounter = (messArray: Message[] | undefined, userID: number) => {
 
-  return (
-    <StyledSwipe>
-      <SwipeableListItem trailingActions={trailingActions()}>
-        <StyledWrapper onClick={handleShowMessageView}>
-          {renderMsgContent}
-          {renderBadge}
-        </StyledWrapper>
-      </SwipeableListItem>
-    </StyledSwipe>
-  );
-};
+      
+      console.log(chat, 'chat')
+      if (chat.messages && chat.messages.slice(-1).pop()?.writer.id != whoAmI.id) {
+        
+        console.log("Du är den som ska få badge");
+        console.log(chat.messages, 'arrayMsg');
+        
+  
+      messArray?.map(c => {
+        !c.isRead ? setCounter(counter + 1) : setCounter(0)
+      })
+      getAllChatMsg(chat?.id);
+      }
+    };
+
+    const trailingActions = () => (
+      <TrailingActions>
+        <SwipeAction
+          destructive={true}
+          onClick={() => console.info("swipe action triggered")}
+        >
+          <StyledDelete>
+            <StyledTrashCan />
+            <StyledDeleteText>Ta bort</StyledDeleteText>
+          </StyledDelete>
+        </SwipeAction>
+      </TrailingActions>
+    );
+
+    const renderMsgContent = (
+      <StyledInnerWrapper>
+        <StyledTitle>{chat.auction.title}</StyledTitle>
+        {chat?.messages && chat.messages.slice(-1).pop() && (
+          <StyledLastMsg>
+            <StyledName>
+              {chat.messages && chat.messages.slice(-1).pop()?.writer.username}:
+            </StyledName>
+            {chat.messages && chat.messages.slice(-1).pop()?.message}
+          </StyledLastMsg>
+        )}
+      </StyledInnerWrapper>
+    );
+
+    const renderBadge = (
+      <StyledBadge badgeContent={counter}>
+        <StyledAvatar>
+          {chat.receiver.username.charAt(0).toLocaleUpperCase()}
+        </StyledAvatar>
+      </StyledBadge>
+    );
+
+    return (
+      <StyledSwipe>
+        <SwipeableListItem trailingActions={trailingActions()}>
+          <StyledWrapper onClick={handleShowMessageView}>
+            {chat.messages && renderMsgContent}
+            {renderBadge}
+          </StyledWrapper>
+        </SwipeableListItem>
+      </StyledSwipe>
+    );
+  };
 
 export default DrawerItem;
--- frontend/src/Contexts/SocketContext.tsx
+++ frontend/src/Contexts/SocketContext.tsx
@@ -37,16 +37,13 @@ const SocketContextProvider = ({ children }: Props) => {
   }, []);
 
   useEffect(() => {
-    console.log("---1.0 ON SOCKET NOTIFICATION ---");
     socket.on("notification", onNotification);
     return () => {
-      console.log("---1.1 OFF SOCKET NOTIFICATION ---");
       socket.off("notification", onNotification);
     };
   }, [socket, whoAmI]);
 
   const onNotification = (data: Notification) => {
-    console.log("---1.2 NOTIFICATION HAPPENED---");
     console.log(whoAmI);
 
     if (whoAmI?.id === data.user.id) {
@@ -56,16 +53,13 @@ const SocketContextProvider = ({ children }: Props) => {
   };
 
   useEffect(() => {
-    console.log("---2.0 ON SOCKET NOTIFICATION ---");
     socket.on("bid", onBid);
     return () => {
-      console.log("---2.1 OFF SOCKET NOTIFICATION ---");
       socket.off("bid", onBid);
     };
   }, [socket]);
 
   const onBid = async (data: BidUpdateSocket) => {
-    console.log("---2.2 BID HAPPENED---");
     const isInDetailView = window.location.href.includes(
       `auctions/${data.auction.id}`
     );
@@ -79,34 +73,32 @@ const SocketContextProvider = ({ children }: Props) => {
   };
 
   useEffect(() => {
-    console.log("---3.0 ON SOCKET NOTIFICATION ---");
+
     socket.on("join", onJoin);
     return () => {
-      console.log("---3.1 OFF SOCKET NOTIFICATION ---");
+
       socket.off("join", onJoin);
     };
   }, [socket]);
 
   const onJoin = (data: number) => {
-    console.log("---3.2 JOIN HAPPENED---");
-    console.log("Client joined the room");
+
     if (data === 2) {
       setIsRead(true);
     }
   };
 
   useEffect(() => {
-    console.log("---4.0 ON SOCKET NOTIFICATION ---");
+
     socket.on("leave", onLeave);
     return () => {
-      console.log("---4.1 OFF SOCKET NOTIFICATION ---");
+
       socket.off("leave", onLeave);
     };
   }, [socket, chatId, getAllChatMsg]);
 
   const onLeave = async (data: number) => {
-    console.log("---4.2 LEAVE HAPPENED---");
-    console.log("Client left room");
+
     if (data === 1) {
       await getAllChatMsg(chatId);
     }
@@ -114,16 +106,13 @@ const SocketContextProvider = ({ children }: Props) => {
   };
 
   useEffect(() => {
-    console.log("---5.0 ON SOCKET NOTIFICATION ---");
     socket.on("message", onMessage);
     return () => {
-      console.log("---5.1 OFF SOCKET NOTIFICATION ---");
       socket.off("message", onMessage);
     };
   }, [socket, chatId, getAllChatMsg]);
 
   const onMessage = async (data: any) => {
-    console.log("---5.2 MESSAGE HAPPENED---");
     
     if (data.id === whoAmI.id) {
       return;
