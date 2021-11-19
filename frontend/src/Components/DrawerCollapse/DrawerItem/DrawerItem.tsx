7c9b1a4d88d121dd984da44cf4102dafd3fc4db6
Ali94160 <ali.al-mousawi@hotmail.com>
2021-11-19 09:40:32 +0100

    fix + cleanups

--- frontend/src/Components/DrawerCollapse/DrawerItem/DrawerItem.tsx
+++ frontend/src/Components/DrawerCollapse/DrawerItem/DrawerItem.tsx
@@ -28,48 +28,22 @@ interface Props {
   chat: Chat;
 }
 
-  const DrawerItem = ({ chat }: Props) => {
-    const { setShowChatRoom } = useDrawer();
-    const { setChatTitle, setChatId, getChatsByCurrentUser } = useChat();
-    const { messages, getAllChatMsg } = useMessage();
-    const { whoAmI } = useAuth();
-    const [counter, setCounter] = useState(0);
-    const { isRead } = useSocket();
+const DrawerItem = ({ chat }: Props) => {
+  const { setShowChatRoom } = useDrawer();
+  const { setChatTitle, setChatId, getChatsByCurrentUser } = useChat();
+  const { getAllChatMsg } = useMessage();
 
-    const handleShowMessageView = () => {
-      setChatId(chat.id);
-      setShowChatRoom(true);
-      setChatTitle(chat.auction.title);
-    };
+  const handleShowMessageView = () => {
+    setChatId(chat.id);
+    setShowChatRoom(true);
+    setChatTitle(chat.auction.title);
+  };
 
-    useEffect(() => {
-      getAllChatMsg(chat?.id);
-      getChatsByCurrentUser();
-    }, []);
+  useEffect(() => {
+    getAllChatMsg(chat?.id);
+    getChatsByCurrentUser();
+  }, []);
 
-    useEffect(() => {
-      badgeCounter(chat.messages, whoAmI.id);
-      console.log(whoAmI.id , 'whoID?')
-    },[setCounter]);
-
-    // chat?.messages[chat?.messages?.length - 1]
-
-    const badgeCounter = (messArray: Message[] | undefined, userID: number) => {
-
-      
-      console.log(chat, 'chat')
-      if (chat.messages && chat.messages.slice(-1).pop()?.writer.id != whoAmI.id) {
-        
-        console.log("Du är den som ska få badge");
-        console.log(chat.messages, 'arrayMsg');
-        
-  
-      messArray?.map(c => {
-        !c.isRead ? setCounter(counter + 1) : setCounter(0)
-      })
-      getAllChatMsg(chat?.id);
-      }
-    };
 
     const trailingActions = () => (
       <TrailingActions>
@@ -100,7 +74,7 @@ interface Props {
     );
 
     const renderBadge = (
-      <StyledBadge badgeContent={counter}>
+      <StyledBadge badgeContent={0}>
         <StyledAvatar>
           {chat.receiver.username.charAt(0).toLocaleUpperCase()}
         </StyledAvatar>
