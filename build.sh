f5052535b47f8641ac9b599c352ece01fae1fb65
Qiling <professorqtaku@gmail.com>
2021-11-30 15:20:40 +0100

    fix heroku port

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ rm -r ./dist
 
 # create dist folder in backend
 # -p  = create all the sub folders
-mkdir -p ./dist/src/main/resources/static
+mkdir -p ./dist/src/main/resources/static/uploads
 
 # build jar file with maven
 mvn clean package
@@ -11,14 +11,17 @@ mvn clean package
 # move jar file to dist folder
 mv ./target/plantrade-0.0.1.jar ./dist/
 
-# build frontend
-cd ../frontend
+cp -r ./src/main/resources/static/uploads/* ./dist/src/main/resources/static/uploads
+cp -r ./plantrade.db ./dist/
+
+## build frontend
+cd ./frontend
 npm run build
 
 
 # move frontend build to static folder
 # move everything in frontend dist folder to backend
-mv ./dist/* ../backend/dist/src/main/resources/static
+mv ./dist/* ../dist/src/main/resources/static
 
 # create script to run jar file
 echo "java -jar plantrade-0.0.1.jar" > ./dist/run.sh
